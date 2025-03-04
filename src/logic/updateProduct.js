0a614f916a7751842d69ee599ddad798292359d7
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-03-04 23:15:51 +0700

    [Add] Demo Data.json
    add demo

---
+++ src/logic/updateProduct.js
@@ -0,0 +1,24 @@
+const updateDemo = (demos, name, newData) => {
+    const index = demos.findIndex(demo => demo.name === name); // ค้นหาชื่อที่ตรงกัน
+    
+    if (index === -1) { // ถ้าไม่พบข้อมูล
+        console.log('demo not found');
+        return null;
+    }
+
+    demos[index] = { ...demos[index], ...newData }; // อัพเดตข้อมูล
+
+    console.log("demos updated successfully:");
+    return demos[index];
+};
+
+let demos = [
+    { name: "kuy", value: "hee" },
+    { name: "ku2y", value: "hee2" }
+];
+
+
+console.log(updateDemo(demos, "ku2y", { name: "IKuy", value: "maeyed" }));
+console.log(demos);
+console.log(data);
+
